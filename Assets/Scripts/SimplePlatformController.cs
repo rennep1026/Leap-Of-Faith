00f8ed2054cdf4da1dacf47b1a54da1240081167
Taylor Penner <rennep1026@gmail.com>
2015-12-01 17:55:33 -0800

    Revert "Fixed Infinite Jump"
    This reverts commit 946f0d2529b393584d03a40d10f45da82d7fe4ae.

--- Assets/Scripts/SimplePlatformController.cs
+++ Assets/Scripts/SimplePlatformController.cs
@@ -13,6 +13,7 @@ public class SimplePlatformController : MonoBehaviour
     public float moveForce = 365f;
     public float maxSpeed = 5f;
     public float jumpForce = 500f;
+    public Transform groundCheck;
     public Text scoreText;
     public int score = 0;
     public AudioSource[] sounds;
@@ -61,10 +62,9 @@ public class SimplePlatformController : MonoBehaviour
     {
         grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
 
-        if (Input.GetButtonDown("Jump") && grounded) // && grounded)
+        if (Input.GetButtonDown("Jump")) // && grounded)
         {
             jump = true;
-			grounded = false;
         }
 
 		if (Input.GetButtonDown ("Fire1")) 
@@ -165,11 +165,5 @@ public class SimplePlatformController : MonoBehaviour
 		}
 		rb2d.velocity = new Vector2 (0, 0);
 		fearValue = 0.5f;
-	}
-
-	void OnCollisionEnter2D (Collision2D other){
-		if (other.gameObject.CompareTag ("ground")) {
-			grounded = true;
-		}
 	}
 }
