51e33150a1991c7d50783ca0fd41b1e83b4b55f6
Taylor Penner <rennep1026@gmail.com>
2015-12-03 20:17:10 -0800

    Started work on waypoint AI and improving tracking AI

--- Assets/Scripts/Enemy_Track.cs
+++ Assets/Scripts/Enemy_Track.cs
@@ -2,10 +2,14 @@
 using System.Collections;
 
 public class Enemy_Track : MonoBehaviour {
+	public Transform groundCheck;
 
+	private float jumpForce = 250f;
+	private bool grounded = false;
 	private SimplePlatformController controller;
 	private float maxSpeed = 3f;
 	private Rigidbody2D rb2d;
+
 
 	// Use this for initialization
 	void Start () {
@@ -13,13 +17,23 @@ public class Enemy_Track : MonoBehaviour {
 		controller = controllerObject.GetComponent<SimplePlatformController>();
 		rb2d = GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Vector3.Distance (controller.transform.position, gameObject.transform.position) < 10) {
-			float directionX = Mathf.Sign(controller.transform.position.x - gameObject.transform.position.x);
-			rb2d.velocity = new Vector2(directionX * maxSpeed, rb2d.velocity.y);
-		}
 
+	void OnCollisionEnter2D (Collision2D other)
+	{ 
+		if (other.gameObject.CompareTag("ground"))
+		{
+			grounded = true;
+		}
+	}
+
+	void FixedUpdate() {
+		if (Vector3.Distance (controller.transform.position, gameObject.transform.position) < 10) {
+			float directionX = Mathf.Sign (controller.transform.position.x - gameObject.transform.position.x);
+			rb2d.velocity = new Vector2 (directionX * maxSpeed, rb2d.velocity.y);
+			if(grounded){
+				rb2d.AddForce(new Vector2(0f, jumpForce));
+				grounded = false;
+			}
+		}
 	}
 }
---
+++ Assets/Scripts/Enemy_Waypoint.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Waypoint : MonoBehaviour {
+
+	public Vector3 patrol1;
+	public Vector3 patrol2;
+
+	private SimplePlatformController controller;
+	private float maxSpeed = 3f;
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		GameObject controllerObject = GameObject.FindWithTag("Player");
+		controller = controllerObject.GetComponent<SimplePlatformController>();
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate () {
+
+	}
+}
